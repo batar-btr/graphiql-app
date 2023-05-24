038713ce9c05ed1288cc0a38e9d848a6109c8230
Alex2 <aleksander1802@gmail.com>
2023-05-24 14:36:16 +0300

    feat: added footer elements

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,28 @@
 import './index.scss';
+import { GithubIcon } from './github.tsx';
+import { RSSchoolIcon } from './rsschool.tsx';
 
 const Footer = () => (
-  <footer>
-    <h1>Footer</h1>
+  <footer className="footer">
+    <div className="footer-rsschool">
+      <a className="footer-rsschool-svg" href="https://rs.school/react/" target="_blanket">
+        <RSSchoolIcon />
+      </a>
+    </div>
+    <div className="footer-creation">
+      <h2>GraphiQL © 2023 Designed by Rick and Morty</h2>
+    </div>
+    <div className="footer-github">
+      <a className="footer-github-svg" href="https://github.com/batar-btr" target="_blanket">
+        <GithubIcon />
+      </a>
+      <a className="footer-github-svg" href="https://github.com/aleksander1802" target="_blanket">
+        <GithubIcon />
+      </a>
+      <a className="footer-github-svg" href="https://github.com/kornienkokostia" target="_blanket">
+        <GithubIcon />
+      </a>
+    </div>
   </footer>
 );
 
