2d66bd31e36efa7aa636a432a61e5b93b740296b
Alex2 <aleksander1802@gmail.com>
2023-05-24 21:04:26 +0300

    feat: updated welcome page

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,29 +1,37 @@
 import './index.scss';
 import { GithubIcon } from './github.tsx';
 import { RSSchoolIcon } from './rsschool.tsx';
+import { useTranslation } from 'react-i18next';
 
-const Footer = () => (
-  <footer className="footer">
-    <div className="footer-rsschool">
-      <a className="footer-rsschool-svg" href="https://rs.school/react/" target="_blanket">
-        <RSSchoolIcon />
-      </a>
-    </div>
-    <div className="footer-creation">
-      <h2>GraphiQL © 2023 Designed by Rick and Morty</h2>
-    </div>
-    <div className="footer-github">
-      <a className="footer-github-svg" href="https://github.com/batar-btr" target="_blanket">
-        <GithubIcon />
-      </a>
-      <a className="footer-github-svg" href="https://github.com/aleksander1802" target="_blanket">
-        <GithubIcon />
-      </a>
-      <a className="footer-github-svg" href="https://github.com/kornienkokostia" target="_blanket">
-        <GithubIcon />
-      </a>
-    </div>
-  </footer>
-);
+const Footer = () => {
+  const { t } = useTranslation();
+  return (
+    <footer className="footer">
+      <div className="footer-rsschool">
+        <a className="footer-rsschool-svg" href="https://rs.school/react/" target="_blanket">
+          <RSSchoolIcon />
+        </a>
+      </div>
+      <div className="footer-creation">
+        <h2>{t('footer')}</h2>
+      </div>
+      <div className="footer-github">
+        <a className="footer-github-svg" href="https://github.com/batar-btr" target="_blanket">
+          <GithubIcon />
+        </a>
+        <a
+          className="footer-github-svg"
+          href="https://github.com/kornienkokostia"
+          target="_blanket"
+        >
+          <GithubIcon />
+        </a>
+        <a className="footer-github-svg" href="https://github.com/aleksander1802" target="_blanket">
+          <GithubIcon />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
